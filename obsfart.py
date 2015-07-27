648d3139a2a063947110927d201aa00545860f8c
Shakna Israel <s4b3r6@gmail.com>
2015-07-28 01:30:43 +1000

    Implemented b()

--- obsfart.py
+++ obsfart.py
@@ -1,4 +1,4 @@
-def a(x=None,y=None):
+def a(x,y=None):
     """Takes two ints, or a list of ints"""
     if y:
         return x.__add__(y)
@@ -6,4 +6,14 @@ def a(x=None,y=None):
         result = 0
         for item in x:
             result = result.__add__(item)
+        return result
+
+def b(x,y=None):
+    """Takes two ints, or a list of ints"""
+    if y:
+        return x.__sub__(y)
+    else:
+        result = 0
+        for item in x:
+            result = result.__sub__(item)
         return result
