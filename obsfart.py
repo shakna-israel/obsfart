f36a6bd8ade7953675c5d3b6faa7e6bc08d81097
Shakna Israel <s4b3r6@gmail.com>
2015-07-28 13:17:55 +1000

    e() implemented for two strings.

--- obsfart.py
+++ obsfart.py
@@ -42,6 +42,40 @@ def d(x,y=None):
 
 def e(x, y=None):
     if y:
+        # Use a for loop to get the index of each character in both strings.
+        # If the index number is a mod (%) of 3, then add it to a list. (One list for x, one for y.)
+        # Rebuild strings from lists, and return both strings.
+        x = list(x)
+        x2 = list(x)
+        y = list(y)
+        x_index = []
+        x_iter = 0
         for i in x:
-            for u in y:
-                raise NotYetImplemented
+            x_iter = x_iter + 1
+            x_index.append(x_iter)
+        y_index = []
+        y_iter = 0
+        for i in y:
+            y_iter = y_iter + 1
+            y_index.append(y_iter)
+        for item in x_index:
+            if not item % 3:
+                try:
+                    replace_with = y[item -1]
+                except IndexError:
+                    replace_with = x[item -1]
+                x2[item -1] = replace_with
+        for item in y_index:
+            if not item % 3:
+                try:
+                    replace_with = x[item -1]
+                except IndexError:
+                    replace_with = y[item -1]
+                y[item -1] = replace_with
+        x = "".join(x2)
+        y = "".join(y)
+        return (x, y)
+    else:
+        raise NotImplementedError
+
+print(e("Herlo","Wolld"))
